8bc10dca5b7d9510cbd682e587817a2ad27fe6cf
MrCode200 <navidyaghmaei@gmail.com>
2024-08-31 13:09:50 +0200

    Improved In correcting test_log_decorator

--- tests/test_log_decorator.py
+++ tests/test_log_decorator.py
@@ -3,8 +3,6 @@ from power_decos import log_decorator
 import json
 import shutil, os
 
-log_decorator.init_logger(True, True, "log", True, True)
-
 
 @log_decorator.log_func()
 def func_log():
@@ -21,17 +19,31 @@ def test_func_log(stdout):
     func_log()
 
     capture = stdout.readout()
+    print(capture)
     assert capture == "l"
 
 
 def test_files_created():
-    # doesnt work
+    # Open the file in read mode
+    with open("tests/logs/log.jsonl", "r") as file:
+        # Initialize a list to hold the JSON objects
+        data = []
+        
+        # Read each line in the file
+        for line in file:
+            try:
+                # Parse the JSON object from the line and append to the list
+                json_object = json.loads(line.strip())
+                data.append(json_object)
+            except json.JSONDecodeError as e:
+                # Print an error message if a line cannot be parsed
+                print(f"Error decoding JSON line: {e}")
 
-    with open("logs/log.jsonl", "r") as file:
-        data = json.load(file)
-
-    print(data[0])
+    # Print the collected data
+    print(data)
 
 
-# test_files_created()
-func_log()
+log_decorator.init_logger(True, True, None, True, True)
+
+test_func_log()
+#test_files_created()
