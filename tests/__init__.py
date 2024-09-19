e67fbbf625fb35b9eeaf3d92f55178e09f9b1577
MrCode200 <navidyaghmaei@gmail.com>
2024-09-19 15:52:37 +0200

    Add initial test instructions to `tests/__init__.py`

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+"""
+### Testing:
+
+PowerDecos requires `pytest`.  Tests can then be run after installation inside the package folder with:
+
+1. run:```pytest tests/test_log_decorator.py```
+2. Excpect: ```3 failed, 10 passed```
+3. run: ```pytest tests/test_log_decorator.py```
+4. Excpect: ```3 passed```
+"""
