eb9d977ff99bcaa6a8312e782cb96e43d38e2d5e
MrCode200 <navidyaghmaei@gmail.com>
2024-09-28 18:19:54 +0200

    Updated documentation

--- .github/generate_docs.bat
+++ .github/generate_docs.bat
@@ -12,6 +12,9 @@ echo Documentation generated successfully.
 robocopy "_build\html" "html" /E /MOVE /R:3 /W:5 /V
 echo HTML files moved successfully(? - throws error dont know why when using || exit /b 1).
 
+rmdir /S /Q "_build\html" || (echo Failed to delete html folder inside _build. Exiting.. & exit/b 1)
+echo Deleted html files inside _build
+
 git commit -a -m "Updated documentation" || (echo Failed to commit changes. Exiting... & exit /b 1)
 echo Changes committed successfully.
 
