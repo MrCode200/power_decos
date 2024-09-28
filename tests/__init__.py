c0cddabb5f3d818e134914da7eda00655cacb63c
MrCode200 <navidyaghmaei@gmail.com>
2024-09-28 22:57:46 +0200

    updated files

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,9 +4,12 @@
 PowerDecos requires `pytest`.  Tests can then be run after installation inside the package folder with:
 
 PowerDecos requires `pytest`. Tests can then be run after installation:
-- run:`pytest`
-- or run: `poetry run pytest`
-- to get a html coverage review add `--cov-report=html` to the end of `pytest`
+
+- run:```pytest```
+
+- or run: ```poetry run pytest```
+
+- to get a html coverage review add ```--cov-report=html``` to the end of ```pytest```
 
 NOTE: if it doesn't work try to go inside the folder and then run the commands
 """
